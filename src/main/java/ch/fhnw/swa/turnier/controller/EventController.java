4fd0a9fe6e4941c78142e9192fef1df74da13bf7
corvus_ch <christian.haeusler@md-systems.ch>
2014-05-23 10:26:40 +0200

    Add team and location selection to events

--- src/main/java/ch/fhnw/swa/turnier/controller/EventController.java
+++ src/main/java/ch/fhnw/swa/turnier/controller/EventController.java
@@ -3,6 +3,8 @@ package ch.fhnw.swa.turnier.controller;
 import ch.fhnw.swa.turnier.beans.CrudBeanInterface;
 import ch.fhnw.swa.turnier.beans.EventBean;
 import ch.fhnw.swa.turnier.domain.Event;
+import ch.fhnw.swa.turnier.domain.EventType;
+import java.util.EnumSet;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -21,5 +23,9 @@ public class EventController extends AbstractController<Event> {
     @Override
     public CrudBeanInterface getBean() {
         return bean;
+    }
+
+    public EnumSet<EventType> getTypes() {
+        return EnumSet.of(EventType.GAME, EventType.TRAINING);
     }
 }
